5d378f632e154611b615c0dccf465dc39ce517d4
Akshay Khobragade <akshaykhobragade@Akshays-MacBook-Air.local>
2020-08-19 22:11:56 +0530

    Github Issues List Proj Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./Styles/App.scss";
+import Navbar from "./Components/Header/navbar";
+import Megamenu from "./Components/Header/megamenu";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Megamenu />
+      <Menu />
     </div>
   );
 }
