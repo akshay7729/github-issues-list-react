830ea8a30212389d6c1b7a85608fdaf3575ec4df
Akshay Khobragade <akshaykhobragade@Akshays-MacBook-Air.local>
2020-08-24 13:06:51 +0530

    Api Patch

--- src/Api/issuesAPI.js
+++ src/Api/issuesAPI.js
@@ -2,11 +2,13 @@ import Axios from "axios";
 
 export const issuesApi = async () => {
   const path = window.location.pathname;
-  const url = window.location.pathname
-    ? `https://api.github.com/repos/${path.split("/")[1]}/${
-        path.split("/")[2]
-      }/issues`
-    : "https://api.github.com/repos/facebook/create-react-app/issues";
+  console.log("path", path.split("/"));
+  const url =
+    path.split("/").length > 2
+      ? `https://api.github.com/repos/${path.split("/")[1]}/${
+          path.split("/")[2]
+        }/issues`
+      : "https://api.github.com/repos/facebook/create-react-app/issues";
   try {
     return Axios.get(url)
       .then((response) => {
